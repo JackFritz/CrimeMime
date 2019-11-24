298f2181667a87c3c38afd4575909ad03814351d
JackFritz <jfritz@quinnipiac.edu>
2019-11-24 13:31:10 -0500

    Shooting Mechanic
    Press Space to Shoot

---
+++ MimeGame/Assets/Scripts/Abilities/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //makes the bullet go ZOOOOOMMMMMMM
+        rb.velocity = transform.right * speed; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        EnemyHealth enemy= hitInfo.GetComponent<EnemyHealth>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(100);
+        }
+        Destroy(gameObject);
+    }
+}
--- MimeGame/Assets/Scripts/Moving.cs
+++ MimeGame/Assets/Scripts/Moving.cs
@@ -33,4 +33,5 @@ public class Moving : MonoBehaviour
      
 
     }
+   
 }
