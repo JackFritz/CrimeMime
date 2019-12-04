014be1d69bf009d9eb2dea40f1d6e1999bb91833
JackFritz <jfritz@quinnipiac.edu>
2019-12-04 11:37:28 -0500

    The PLOT

---
+++ MimeGame/Assets/Scripts/Menus/Plot4Button.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Plot4Button : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Plot5");
+    }
+}
